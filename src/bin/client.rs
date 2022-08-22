045f0af5d24efdb5e5479a52ce65c8e50a6cd177
ChenYing Kuo <evshary@gmail.com>
2022-08-22 17:24:54 +0800

    Able to send to/back message between server & client.

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,8 +1,16 @@
 use std::env;
+use std::net::TcpStream;
+use std::{thread, time};
+use std::sync::mpsc;
+use std::str;
+use std::io::{self, Read, Write};
+
+const MSG_SIZE: usize = 100;
 
 fn main() {
     println!("Chatroom client:");
 
+    // Parse arguments
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         println!("Usage: client <IP> <port>");
@@ -11,4 +19,44 @@ fn main() {
     let ip = &args[1];
     let port: u32 = args[2].parse().unwrap();
     println!("Connect to {}:{}", ip, port);
+
+    // Setup connection
+    let addr = format!("{}:{}", ip, port);
+    let mut stream = TcpStream::connect(addr).expect("Unable to connect");
+    stream.set_nonblocking(true).expect("Unable to set nonblocking");
+    println!("Connecting successfully");
+    let (tx, rx) = mpsc::channel::<String>();
+
+    // Create recv & send thread
+    thread::spawn(move || {
+        let mut buf = [0; MSG_SIZE];
+        loop {
+            // Read data from server
+            match stream.read(&mut buf) {
+                Ok(size) => {
+                    if size != 0 {
+                        let msg = str::from_utf8(&buf[..size]).unwrap();
+                        print!("{}", msg);
+                        io::stdout().flush().unwrap();  // Need to flush the stdout
+                    }
+                },
+                Err(_) => {}
+            }
+            // Read data from user's input
+            match rx.try_recv() {
+                Ok(msg) => {
+                    stream.write_all(msg.as_bytes()).expect("Unable to send message");
+                },
+                Err(_) => {}
+            }
+            thread::sleep(time::Duration::from_millis(10));
+        }
+    });
+
+    // Get user input
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Not correct input");
+        tx.send(input).expect("Internal error");
+    }
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,16 @@
 use std::env;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
+use std::io::{self, Read, Write};
+
+const MSG_SIZE: usize = 100;
 
 fn main() {
     println!("Chatroom server:");
 
+    // Parse arguments
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Usage: server <port>");
@@ -10,4 +18,47 @@ fn main() {
     }
     let port: u32 = args[1].parse().unwrap();
     println!("Listening to port {}", port);
+
+    // Setup server listener
+    let addr = format!("127.0.0.1:{}", port);
+    let listener = TcpListener::bind(addr).unwrap();
+    let (tx, rx) = mpsc::channel::<String>();
+    let senders = Arc::new(Mutex::new(vec![]));
+    {
+        let senders: Arc<Mutex<Vec<TcpStream>>> = senders.clone();
+        thread::spawn(move || loop {
+            let msg = rx.recv().unwrap();
+            print!("{}", msg);
+            io::stdout().flush().unwrap();  // Need to flush the stdout
+            for s in senders.lock().unwrap().iter_mut() {
+                s.write_all("abc123".as_bytes()).expect("Unable to write");
+            }
+        });
+    }
+
+    // Recv connection from client
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                senders.lock().unwrap().push(stream.try_clone().unwrap());
+                println!("Connected by {}", stream.peer_addr().unwrap());
+                let tx = tx.clone();
+                thread::spawn(move || {
+                    let mut buf = [0; MSG_SIZE];
+                    loop {
+                        match stream.read(&mut buf) {
+                            Ok(size) => {
+                                let msg = String::from_utf8(buf[..size].to_vec()).expect("Invalid message");
+                                tx.send(msg).expect("Internal error");
+                            },
+                            Err(_) => {}
+                        }
+                    }
+                });
+            }
+            Err(e) => {
+                println!("Error connection: {}", e);
+            }
+        }
+    }
 }
